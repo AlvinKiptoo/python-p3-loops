9a2a2e04df77e43361744141b4cfc671cc3f068a
Alvin Kiptoo <kiptooalvin10@gmail.com>
2024-12-03 16:13:12 +0300

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -12,14 +12,16 @@ def square_integers(int_list):
     # code goes here!
     return [x ** 2 for x in int_list]
 
-def fizzbuzz(num):
+def fizzbuzz():
     # code goes here!
-    for num in range(100):
-        if num % 3 == 0 and num % 5 == 0:
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz")
-        elif num % 3 == 0:
+        elif i % 3 == 0:
             print("Fizz")
-        elif num % 5 == 0:
+        elif i % 5 == 0:
             print("Buzz")
         else:
-            print(num)
+            print(i)
+
+fizzbuzz()
